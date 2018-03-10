6e9219155921edd41d096c9047d6a9244a0a5413
Tjian Lam <tjian.lam@thinkbiganalytics.com>
2018-03-11 00:55:43 +0100

    added geo plot

--- scrape_wiki_european_cities.R
+++ scrape_wiki_european_cities.R
@@ -58,7 +58,65 @@ df.euro_cities <- df.euro_cities %>% as_tibble()
 #   using ggmap::geocode()
 #========================================================
 
-data.geo <- data.frame(city=df.euro_cities$city,geocode(df.euro_cities$city))
-t1<-data.geo %>% filter(data.geo$lon %>% is.na())  %>% select(city)
+data.geo <- data.frame(city=df.euro_cities$city,geocode(df.euro_cities$city),stringsAsFactors = FALSE)
+data.geo_retry <- data.geo %>% filter(data.geo$lon %>% is.na())
+data.geo_retry <- data.frame(city=data.geo_retry$city,geocode(data.geo_retry$city),stringsAsFactors = FALSE)
 
+data.geo[data.geo$lon %>% is.na(),] <- data.geo_retry
 data.geo %>% head()
+
+df.euro_cities <- merge(df.euro_cities, data.geo, by='city', all = TRUE)
+
+#==============
+# GET WORLD MAP
+#==============
+
+map.europe <- map_data("world")
+
+#=================================
+# PLOT BASIC MAP
+# - this map is "just the basics"
+#=================================
+
+ggplot() +
+  geom_polygon(data = map.europe, aes(x = long, y = lat, group = group)) +
+  geom_point(data = df.euro_cities, aes(x = lon, y = lat, size = population), color = "red", alpha = .3) +
+  coord_cartesian(xlim = c(-9,45), ylim = c(32,70))
+
+#====================================================
+# PLOT 'POLISHED' MAP
+# - this version is formatted and cleaned up a little
+#   just to make it look more aesthetically pleasing
+#====================================================
+
+#-------------
+# CREATE THEME
+#-------------
+theme.maptheeme <-
+  theme(text = element_text(family = "Gill Sans", color = "#444444")) +
+  theme(plot.title = element_text(size = 32)) +
+  theme(plot.subtitle = element_text(size = 16)) +
+  theme(panel.grid = element_blank()) +
+  theme(axis.text = element_blank()) +
+  theme(axis.ticks = element_blank()) +
+  theme(axis.title = element_blank()) +
+  theme(legend.background = element_blank()) +
+  theme(legend.key = element_blank()) +
+  theme(legend.title = element_text(size = 18)) +
+  theme(legend.text = element_text(size = 10)) +
+  theme(panel.background = element_rect(fill = "#596673")) +
+  theme(panel.grid = element_blank())
+
+
+#------
+# PLOT
+#------
+#fill = "#AAAAAA",colour = "#818181", size = .15)
+ggplot() +
+  geom_polygon(data = map.europe, aes(x = long, y = lat, group = group), fill = "#DEDEDE",colour = "#818181", size = .15) +
+  geom_point(data = df.euro_cities, aes(x = lon, y = lat, size = population), color = "red", alpha = .3) +
+  geom_point(data = df.euro_cities, aes(x = lon, y = lat, size = population), color = "red", shape = 1) +
+  coord_cartesian(xlim = c(-9,45), ylim = c(32,70)) +
+  labs(title = "European Cities with Large Populations", subtitle = "Cities with over 1MM population, within city limits") +
+  scale_size_continuous(range = c(.7,15), breaks = c(1100000, 4000000, 8000000, 12000000), name = "Population", labels = scales::comma_format()) +
+  theme.maptheeme
